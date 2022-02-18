184379da9676ecb81a311dd76a73eed43a339040
Vatsal <chauhanvatsal55@gmail.com>
2022-02-18 18:52:52 +0530

    feat: header & search component staged

---
+++ pages/vatsal/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <TheHeader />
+    <the-search />
+  </div>
+</template>
+
+<script>
+import TheHeader from '~/components/TheHeader.vue'
+import TheSearch from '~/components/TheSearch.vue'
+export default { components: { TheHeader, TheSearch } }
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Karla:wght@500&family=Rubik:wght@400;500;700&display=swap');
+
+* {
+  box-sizing: border-box;
+  font-family: 'Nunito Sans', sans-serif;
+  font-weight: 700;
+  margin: 0;
+  padding: 0;
+}
+</style>
