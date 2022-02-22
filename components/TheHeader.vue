a348bf6bb346414059954f64054ea7cba60f14e0
Vatsal <87218847+codesnail21@users.noreply.github.com>
2022-02-22 15:11:16 +0530

    feat: header & search component staged (#8)
    * feat: header & search component staged
    
    * feat: reomved un wanted things

---
+++ components/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="head navbar align-content-center">
+    <h1>Where in the world?</h1>
+    <p class="">Dark Mode</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.head {
+  /* margin-left: 2em; */
+  flex: auto;
+  padding: 1em;
+  background-color: whitesmoke;
+  padding-left: 3em;
+  padding-right: 3em;
+}
+</style>
